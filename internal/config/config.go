82c414cee7ee8afc23d49d53ba2bb6d697600938
VaudKK <vaudkeith@gmail.com>
2026-03-19 14:04:20 +0300

    fix: conditional loading of .env

--- internal/config/config.go
+++ internal/config/config.go
@@ -16,11 +16,14 @@ type Config struct {
 }
 
 func LoadConfig(log *logrus.Logger) *Config {
-	err := godotenv.Load()
-	if err != nil {
-		log.WithError(err).Error("Error while loading configs")
-		return nil
+	if os.Getenv("APP_ENV") == "development"{
+		err := godotenv.Load()
+		if err != nil {
+			log.WithError(err).Error("Error while loading configs")
+			return nil
+		}
 	}
+	
 
 	return &Config{
 		AwsAccessKey:       os.Getenv("AWS_ACCESS_KEY_ID"),
